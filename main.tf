7ec5266dda936466a088096dd9a251329db85625
aryakommineni <147579531+aryakommineni@users.noreply.github.com>
2025-09-30 10:51:08 -0700

    main.tf

--- main.tf
+++ main.tf
@@ -17,15 +17,6 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = var.vm_name
   machine_type = var.machine_type
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {}
-  }
+  boot_disk { initialize_params { image = "debian-cloud/debian-11" } }
+  network_interface { network = "default" access_config {} }
 }
