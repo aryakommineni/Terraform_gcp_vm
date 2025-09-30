f4fec5eda7ed13d9427bd5b8a2cbbccb75b76a60
aryakommineni <147579531+aryakommineni@users.noreply.github.com>
2025-09-30 13:32:30 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "google" {
+  credentials = file(var.credentials_file)
   project     = var.project_id
   region      = var.region
   zone        = var.zone
-  credentials = file(var.credentials_file)
 }
 
-variable "project_id" {}
-variable "region" {}
-variable "zone" {}
-variable "vm_name" {}
-variable "machine_type" {}
-variable "credentials_file" {}
-
-resource "google_compute_instance" "default" {
+resource "google_compute_instance" "vm_instance" {
   name         = var.vm_name
   machine_type = var.machine_type
   zone         = var.zone
@@ -24,10 +26,7 @@ resource "google_compute_instance" "default" {
   }
 
   network_interface {
- HEAD
     network       = "default"
-    network = "default"
- c62d5122e2d8aaf5c88f22f1d6a1aa1a4628f6d9
     access_config {}
   }
 }
@@ -35,4 +34,3 @@ resource "google_compute_instance" "default" {
 output "instance_name" {
   value = google_compute_instance.vm_instance.name
 }
-
