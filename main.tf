f75ce718b49aca8eae9f39236d55c739ee11041c
root <root@instance-1.us-west2-a.c.optical-caldron-472316-u5.internal>
2025-09-30 15:28:26 -0400

    Fix boot_disk block in main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,21 @@ provider "google" {
 resource "google_compute_instance" "vm_instance" {
   name         = var.vm_name
   machine_type = var.machine_type
-  boot_disk { initialize_params { image = "debian-cloud/debian-11" } }
-  network_interface { network = "default" access_config {} }
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
 }
+
+output "instance_name" {
+  value = google_compute_instance.vm_instance.name
+}
+
