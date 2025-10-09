30e3408a6df55bf23c95453ac9ce36ff9bf599b3
aryakommineni <147579531+aryakommineni@users.noreply.github.com>
2025-10-09 13:34:18 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "instance_self_link" {
-  value = google_compute_instance.vm_instance.self_link
-}
-
+output "self_link"   { value = google_compute_instance.vm.self_link }
+output "name"        { value = google_compute_instance.vm.name }
+output "zone"        { value = google_compute_instance.vm.zone }
+output "external_ip" { value = try(google_compute_instance.vm.network_interface[0].access_config[0].nat_ip, null) }
